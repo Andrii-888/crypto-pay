0fc50861cc2af6d937bc1daa2e447dcbdc750db5
Andrii-888 <122465780+Andrii-888@users.noreply.github.com>
2025-11-27 18:52:37 +0100

    Rename next.config.ts to next.config.mjs and add i18n

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  i18n: {
+    locales: ["en", "it", "de", "fr", "ru"],
+    defaultLocale: "en",
+  },
+};
+
+export default nextConfig;
